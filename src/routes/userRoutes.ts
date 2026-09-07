5b3ce64b4701fe33237f0981d3fadeb94ddbee28
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-07 13:58:37 +0500

    Remove broken dashboard route reference

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getMyProfile, getPublicProfile, postProfileSetup, patchMyProfile, searchUsersHandler, getMyDashboard } from '../controllers/userController';
+import { getMyProfile, getPublicProfile, postProfileSetup, patchMyProfile, searchUsersHandler } from '../controllers/userController';
 import { authMiddleware } from '../middleware/authMiddleware';
 import { optionalAuth } from '../middleware/optionalAuth';
 
@@ -7,7 +7,6 @@ const router = Router();
 router.get('/me', authMiddleware, getMyProfile);
 router.patch('/me', authMiddleware, patchMyProfile);
 router.post('/me/profile-setup', authMiddleware, postProfileSetup);
-router.get('/me/dashboard', authMiddleware, getMyDashboard);
 router.get('/search', searchUsersHandler);
 router.get('/:username', optionalAuth, getPublicProfile);
 
