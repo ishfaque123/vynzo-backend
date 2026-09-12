8c5ff4ebc0376ada99f6451fc723b2d2b5a5686c
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-12 15:46:28 +0500

    fix: remove duplicate savePublicKeyHandler

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -112,16 +112,6 @@ export async function savePublicKeyHandler(req: Request, res: Response, next: Ne
   }
 }
 
-export async function savePublicKeyHandler(req: Request, res: Response, next: NextFunction) {
-  try {
-    const { publicKey } = req.body as { publicKey?: string };
-    if (!publicKey) throw new (await import('../middleware/errorHandler')).ApiError(400, 'NO_KEY', 'publicKey is required.');
-    await prisma.user.update({ where: { id: req.user!.id }, data: { publicKey } });
-    sendSuccess(res, { saved: true });
-  } catch (err) {
-    next(err);
-  }
-}
 
 export async function logout(_req: Request, res: Response) {
   clearAuthCookies(res);
