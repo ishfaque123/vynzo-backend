e742a04c59ed17e3e4669be6c553827aea4632e2
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-15 07:11:45 +0500

    fix(reels): add comment report route

--- src/routes/reelRoutes.ts
+++ src/routes/reelRoutes.ts
@@ -14,6 +14,7 @@ import {
   getReelCommentsHandler,
   toggleReelCommentReactionHandler,
   editReelCommentHandler,
+  reportReelCommentHandler,
   deleteReelCommentHandler,
 } from '../controllers/reelController';
 
@@ -30,6 +31,7 @@ router.get('/:id/comments', authMiddleware, getReelCommentsHandler);
 router.post('/:id/comments', authMiddleware, addReelCommentHandler);
 router.post('/comments/:commentId/reaction', authMiddleware, toggleReelCommentReactionHandler);
 router.put('/comments/:commentId', authMiddleware, editReelCommentHandler);
+router.post('/comments/:commentId/report', authMiddleware, reportReelCommentHandler);
 router.delete('/comments/:commentId', authMiddleware, deleteReelCommentHandler);
 
 export default router;
